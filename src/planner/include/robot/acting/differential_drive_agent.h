307449c9282181dcf5f79f0863148800b6bb762f
Yashasvi Sriram Patkuri <patku001@umn.edu>
2020-04-26 21:39:21 -0500

    impls orienting towards next milestone before going forward, draws orientation in rviz, sends orientation to gazebo

--- src/planner/include/robot/acting/differential_drive_agent.h
+++ src/planner/include/robot/acting/differential_drive_agent.h
@@ -10,24 +10,28 @@ using namespace ros;
 using namespace Eigen;
 
 struct DifferentialDriveAgent {
-    const float MILESTONE_REACHED_RADIUS = 0.01f;
+    const float MILESTONE_SLACK = 0.01f;
+    const float ORIENTATION_SLACK = 0.01f;
     const Publisher& rviz;
     const Publisher& gazebo;
-    Vector2f position;
+    Vector2f center;
     float orientation;
     float radius;
     vector<Vector2f> path;
     int current_milestone = 0;
-    float speed;
+    float linear_speed;
+    float angular_speed;
 
     DifferentialDriveAgent(const Publisher& rviz,
                            const Publisher& gazebo,
-                           Vector2f position,
+                           Vector2f center,
                            float orientation,
                            float radius,
-                           float speed): 
-        rviz(rviz), gazebo(gazebo), position(position), orientation(orientation), radius(radius), speed(speed) {
-        path.push_back(position);
+                           float linear_speed,
+                           float angular_speed): 
+        rviz(rviz), gazebo(gazebo), center(center), orientation(orientation), radius(radius),
+            linear_speed(linear_speed), angular_speed(angular_speed) {
+        path.push_back(center);
     }
 
     void set_path(vector<Vector2f> new_path) {
@@ -36,45 +40,82 @@ struct DifferentialDriveAgent {
 
     void update(float dt, const ConfigurationSpace& cs) {
         if (current_milestone < path.size() - 1) {
-            // reached next milestone
-            if ((path[current_milestone + 1] - position).norm() < MILESTONE_REACHED_RADIUS) {
+            Vector2f to_goal = path[current_milestone + 1] - center;
+            float goal_orientation = atan2(to_goal[1], to_goal[0]);
+            float to_orientation = goal_orientation - orientation;
+            // Orient towards goal
+            if (abs(to_orientation) > ORIENTATION_SLACK) {
+                orientation += (to_orientation > 0) ? angular_speed * dt : -angular_speed * dt;
+                return;
+            }
+            // Reached next milestone
+            if (to_goal.norm() < MILESTONE_SLACK) {
                 current_milestone++;
                 return;
             }
-            // move towards next milestone
-            Vector2f velocity_dir = (path[current_milestone + 1] - position).normalized();
-            Vector2f displacement = velocity_dir * speed * dt;
-            position += displacement;
+            // Move towards next milestone
+            Vector2f velocity_dir = to_goal.normalized();
+            Vector2f displacement = velocity_dir * linear_speed * dt;
+            center += displacement;
         }
     }    
 
     void draw_rviz() {
-        visualization_msgs::Marker agent;
-        agent.header.frame_id = "/map";
-        agent.ns = "agent";
-        agent.header.stamp = ros::Time::now();
-        agent.id = 0;
-        agent.type = visualization_msgs::Marker::CYLINDER;
-        agent.action = visualization_msgs::Marker::ADD;
-        agent.pose.position.x = position[0];
-        agent.pose.position.y = position[1];
-        agent.pose.orientation.w = 1.0;
-        agent.scale.x = radius;
-        agent.scale.y = radius;
-        agent.scale.z = 0.01;
-        agent.color.r = 1.0f;
-        agent.color.g = 1.0f;
-        agent.color.b = 1.0f;
-        agent.color.a = 1.0;
-        rviz.publish(agent);
+        // Position
+        visualization_msgs::Marker center_marker;
+        center_marker.header.frame_id = "/map";
+        center_marker.ns = "agent";
+        center_marker.header.stamp = ros::Time::now();
+        center_marker.id = 0;
+        center_marker.type = visualization_msgs::Marker::CYLINDER;
+        center_marker.action = visualization_msgs::Marker::ADD;
+        center_marker.pose.position.x = center[0];
+        center_marker.pose.position.y = center[1];
+        center_marker.pose.orientation.w = 1.0;
+        center_marker.scale.x = 2 * radius;
+        center_marker.scale.y = 2 * radius;
+        center_marker.scale.z = 0.01;
+        center_marker.color.r = 1.0f;
+        center_marker.color.g = 1.0f;
+        center_marker.color.b = 1.0f;
+        center_marker.color.a = 1.0;
+        rviz.publish(center_marker);
+
+        // Orientation
+        visualization_msgs::Marker orientation_marker;
+        orientation_marker.header.frame_id = "/map";
+        orientation_marker.ns = "agent";
+        orientation_marker.header.stamp = ros::Time::now();
+        orientation_marker.id = 1;
+        orientation_marker.action = visualization_msgs::Marker::ADD;
+        orientation_marker.pose.orientation.w = 1.0;
+        orientation_marker.color.a = 1.0;
+        orientation_marker.type = visualization_msgs::Marker::LINE_LIST;
+        orientation_marker.scale.x = 0.01;
+        orientation_marker.color.r = 1.0f;
+
+        geometry_msgs::Point p1;
+        p1.x = center[0];
+        p1.y = center[1];
+        p1.z = 0.2;
+        orientation_marker.points.push_back(p1);
+        geometry_msgs::Point p2;
+        p2.x = center[0] + cos(orientation) * radius;
+        p2.y = center[1] + sin(orientation) * radius;
+        p2.z = 0.2;
+        orientation_marker.points.push_back(p2);
+
+        rviz.publish(orientation_marker);
+
     }
 
     void draw_gazebo() {
         gazebo_msgs::ModelState tutlebot_state;
         tutlebot_state.model_name = "turtlebot3_burger";
-        tutlebot_state.pose.position.x = position[0];
-        tutlebot_state.pose.position.y = position[1];
-        tutlebot_state.pose.orientation.w = 1;
+        tutlebot_state.pose.position.x = center[0];
+        tutlebot_state.pose.position.y = center[1];
+        tutlebot_state.pose.orientation.z = sin(orientation / 2);
+        tutlebot_state.pose.orientation.w = cos(orientation / 2);
         gazebo.publish(tutlebot_state);
     }
 
--- src/planner/src/planner_node.cpp
+++ src/planner/src/planner_node.cpp
@@ -12,6 +12,8 @@ using namespace std;
 using namespace ros;
 using namespace Eigen;
 
+const float PI = 3.141592653589793;
+
 int main(int argc, char **argv) {
     init(argc, argv, "planner");
     NodeHandle node_handle;
@@ -24,13 +26,13 @@ int main(int argc, char **argv) {
     room.add_wall(Vector2f(-3, 3), Vector2f(-3, -3));
     room.add_wall(Vector2f(-3, -3), Vector2f(1, -3));
 
-    Publisher gazebo = node_handle.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 1000);
-    DifferentialDriveAgent turtle(rviz, gazebo, Vector2f(0.0, 0.0), 0.0, 0.4, 5);
+    Publisher gazebo = node_handle.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 10000);
+    DifferentialDriveAgent turtle(rviz, gazebo, Vector2f(0.0, 0.0), PI / 4, 0.4, 5, 5);
 
     ConfigurationSpace cs(rviz, room, turtle.radius + 0.1);
 
-    ProbabilisticRoadmap prm(rviz, 1000, Vector2f(-5, -5), Vector2f(5, 5), 0.6, cs);
-    vector<Vector2f> path = prm.bfs(Vector2f(turtle.position[0], turtle.position[1]), Vector2f(-5.0, 5.0), 1, cs);
+    ProbabilisticRoadmap prm(rviz, 1500, Vector2f(-5, -5), Vector2f(5, 5), 0.5, cs);
+    vector<Vector2f> path = prm.bfs(Vector2f(turtle.center[0], turtle.center[1]), Vector2f(-5.0, 5.0), 1, cs);
     turtle.set_path(path);
 
     while (ros::ok()) {
@@ -44,9 +46,9 @@ int main(int argc, char **argv) {
         prm.draw_milestones();
         room.draw();
         cs.draw();
-        turtle.draw_rviz();
-        turtle.draw_path();
-        // turtle.draw_gazebo();
+        // turtle.draw_rviz();
+        // turtle.draw_path();
+        turtle.draw_gazebo();
 
         /* Sleep */
         rate.sleep();
