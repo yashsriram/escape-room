57cbdb6f6ea93a646e72887c355795ffc27ac611
Yashasvi Sriram Patkuri <patku001@umn.edu>
2020-04-26 20:09:38 -0500

    impl agent path setting and following 	impls set_path(), update(), draw_rviz(), draw_path() in DifferentialDriveAgent{} 	tunes update dt using for loop

--- src/planner/include/robot/acting/differential_drive_agent.h
+++ src/planner/include/robot/acting/differential_drive_agent.h
@@ -3,22 +3,53 @@
 
 #include <eigen3/Eigen/Dense>
 #include <ros/ros.h>
+#include "robot/sensing/configuration_space.h"
 
 using namespace std;
 using namespace ros;
 using namespace Eigen;
 
 struct DifferentialDriveAgent {
-	const Publisher& rviz;
-	Vector2f position;
-	float orientation;
-	float radius;
+    const float MILESTONE_REACHED_RADIUS = 0.01f;
+    const Publisher& rviz;
+    const Publisher& gazebo;
+    Vector2f position;
+    float orientation;
+    float radius;
+    vector<Vector2f> path;
+    int current_milestone = 0;
+    float speed;
 
-	DifferentialDriveAgent(const Publisher& rviz, Vector2f position, float orientation, float radius): 
-		rviz(rviz), position(position), orientation(orientation), radius(radius) {}
+    DifferentialDriveAgent(const Publisher& rviz,
+                           const Publisher& gazebo,
+                           Vector2f position,
+                           float orientation,
+                           float radius,
+                           float speed): 
+        rviz(rviz), gazebo(gazebo), position(position), orientation(orientation), radius(radius), speed(speed) {
+        path.push_back(position);
+    }
 
-	void draw() {
-		visualization_msgs::Marker agent;
+    void set_path(vector<Vector2f> new_path) {
+        path = new_path;
+    }
+
+    void update(float dt, const ConfigurationSpace& cs) {
+        if (current_milestone < path.size() - 1) {
+            // reached next milestone
+            if ((path[current_milestone + 1] - position).norm() < MILESTONE_REACHED_RADIUS) {
+                current_milestone++;
+                return;
+            }
+            // move towards next milestone
+            Vector2f velocity_dir = (path[current_milestone + 1] - position).normalized();
+            Vector2f displacement = velocity_dir * speed * dt;
+            position += displacement;
+        }
+    }    
+
+    void draw_rviz() {
+        visualization_msgs::Marker agent;
         agent.header.frame_id = "/map";
         agent.ns = "agent";
         agent.header.stamp = ros::Time::now();
@@ -36,7 +67,42 @@ struct DifferentialDriveAgent {
         agent.color.b = 1.0f;
         agent.color.a = 1.0;
         rviz.publish(agent);
-	}
+    }
+
+    void draw_gazebo() {
+        gazebo_msgs::ModelState tutlebot_state;
+        tutlebot_state.model_name = "turtlebot3_burger";
+        tutlebot_state.pose.position.x = position[0];
+        tutlebot_state.pose.position.y = position[1];
+        tutlebot_state.pose.orientation.w = 1;
+        gazebo.publish(tutlebot_state);
+    }
+
+
+    void draw_path() {
+        visualization_msgs::Marker path_marker;
+        path_marker.header.frame_id = "/map";
+        path_marker.ns = "path";
+        path_marker.header.stamp = ros::Time::now();
+        path_marker.id = 0;
+        path_marker.action = visualization_msgs::Marker::ADD;
+        path_marker.pose.orientation.w = 1.0;
+        path_marker.color.a = 1.0;
+        path_marker.type = visualization_msgs::Marker::LINE_STRIP;
+        path_marker.scale.x = 0.01;
+        path_marker.color.r = path_marker.color.g = path_marker.color.b = 1.0f;
+
+        // Links
+        for (const auto &milestone : path) {
+            geometry_msgs::Point p;
+            p.x = milestone[0];
+            p.y = milestone[1];
+            path_marker.points.push_back(p);
+        }
+
+        rviz.publish(path_marker);
+    }
+
 };
 
 
--- src/planner/src/planner_node.cpp
+++ src/planner/src/planner_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <gazebo_msgs/ModelState.h>
 
 #include "robot/sensing/room.h"
 #include "robot/sensing/configuration_space.h"
@@ -11,35 +12,11 @@ using namespace std;
 using namespace ros;
 using namespace Eigen;
 
-void draw_path(const Publisher& rviz, const vector<Vector2f>& path) {
-    visualization_msgs::Marker path_marker;
-    path_marker.header.frame_id = "/map";
-    path_marker.ns = "path";
-    path_marker.header.stamp = ros::Time::now();
-    path_marker.id = 0;
-    path_marker.action = visualization_msgs::Marker::ADD;
-    path_marker.pose.orientation.w = 1.0;
-    path_marker.color.a = 1.0;
-    path_marker.type = visualization_msgs::Marker::LINE_STRIP;
-    path_marker.scale.x = 0.01;
-    path_marker.color.r = path_marker.color.g = path_marker.color.b = 1.0f;
-
-    // Links
-    for (const auto &milestone : path) {
-        geometry_msgs::Point p;
-        p.x = milestone[0];
-        p.y = milestone[1];
-        path_marker.points.push_back(p);
-    }
-
-    rviz.publish(path_marker);
-}
-
 int main(int argc, char **argv) {
     init(argc, argv, "planner");
     NodeHandle node_handle;
     Publisher rviz = node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 10000);
-    Rate rate(10);
+    Rate rate(20);
 
     Room room(rviz);
     room.add_wall(Vector2f(3, 3), Vector2f(3, -3));
@@ -47,23 +24,29 @@ int main(int argc, char **argv) {
     room.add_wall(Vector2f(-3, 3), Vector2f(-3, -3));
     room.add_wall(Vector2f(-3, -3), Vector2f(1, -3));
 
-    DifferentialDriveAgent turtle(rviz, Vector2f(0.0, 0.0), 0.0, 0.4);
+    Publisher gazebo = node_handle.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 1000);
+    DifferentialDriveAgent turtle(rviz, gazebo, Vector2f(0.0, 0.0), 0.0, 0.4, 5);
 
     ConfigurationSpace cs(rviz, room, turtle.radius + 0.1);
 
     ProbabilisticRoadmap prm(rviz, 1000, Vector2f(-5, -5), Vector2f(5, 5), 0.6, cs);
-    vector<Vector2f> path = prm.bfs(Vector2f(0.0, 0.0), Vector2f(-5.0, 5.0), 1, cs);
+    vector<Vector2f> path = prm.bfs(Vector2f(turtle.position[0], turtle.position[1]), Vector2f(-5.0, 5.0), 1, cs);
+    turtle.set_path(path);
 
     while (ros::ok()) {
         /* Update */
-
+        for (int i = 0; i < 10; ++i) {
+            turtle.update(0.001, cs);
+        }
+        
         /* Draw */
         prm.draw_links();
         prm.draw_milestones();
         room.draw();
         cs.draw();
-        turtle.draw();
-        draw_path(rviz, path);
+        turtle.draw_rviz();
+        turtle.draw_path();
+        // turtle.draw_gazebo();
 
         /* Sleep */
         rate.sleep();
